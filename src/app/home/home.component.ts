25c9152c966f6e85f6eca382f4688fac7aa69c0e
4Gatos <4gatosphotovideo@gmail.com>
2022-05-13 01:10:21 +0200

    Route animations fixed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+// jQuery
+declare var $: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  private fragment: string;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.fragment.subscribe(fragment => {
+      this.fragment = fragment;
+    });
   }
+
+  ngAfterViewInit(): void {
+    try {
+      document.querySelector('#' + this.fragment).scrollIntoView();
+      window.scrollBy(0,-60)
+      history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search);
+    } catch (e) {}
+  }
+  
 
 }
--- src/app/session-page/session-page.component.ts
+++ src/app/session-page/session-page.component.ts
@@ -1,4 +1,4 @@
-import { Attribute, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Attribute, Component, OnInit } from '@angular/core';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import { Session } from './session';
@@ -9,7 +9,7 @@ declare var $: any;
   templateUrl: './session-page.component.html',
   styleUrls: ['./session-page.component.scss']
 })
-export class SessionPageComponent implements OnInit {
+export class SessionPageComponent implements OnInit, AfterViewInit {
 
   faWhatsapp = faWhatsapp;
   faPlus = faPlus;
@@ -26,6 +26,16 @@ export class SessionPageComponent implements OnInit {
   constructor() {
     
   }
+  ngAfterViewInit(): void {
+    let timeout;
+    if (!timeout) {
+      timeout = setTimeout(() => {
+        window.scroll({ top: 0, left: 0, behavior: 'auto'});
+        timeout = null;
+      }, 300)
+    }
+  }
+
   ngOnInit(): void {
     this.galleryData = {
       id: this.data.id,
